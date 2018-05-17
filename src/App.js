fabc081da956dc0b8d78a97c19288f542774fc56
ipakhotin <xxoTkPoBeHuExx@gmail.com>
2018-05-17 22:13:41 +0600

    better

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import Login from './components/login/login.jsx';
+import Main from './components/main/main.jsx';
+import Profile from './components/profile/profile.jsx';
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.renderHref = this.renderHref.bind(this);
+	}
+	getNavigationList() {
+		return [{ text: 'На главную', href: '' }, { text: 'Новости', href: '' }, { text: 'Профиль', href: '' }];
+	}
+	renderHref({ href, text }, key) {
+		return (
+			<li key={text}>
+				<a href={href}>{text}</a>
+			</li>
+		);
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<h1 className="App-title">Welcome to React</h1>
+					<ul>{this.getNavigationList().map(this.renderHref)}</ul>
 				</header>
-				<Login />
+				<Main />
 			</div>
 		);
 	}
